c75ec28d967e60ebfb076ce9535c560b8aaf92b6
kedar <kedarmail@gmail.com>
2020-07-12 22:05:59 -0700

    bug fixes

--- phoneapp/screens/UnlockDevice/unlockDevice.js
+++ phoneapp/screens/UnlockDevice/unlockDevice.js
@@ -26,7 +26,6 @@ export default class unlockDevice extends React.Component {
         this.state = {
             device: props,
             connected: 0, // 0=no, 1=yes, 2=failed
-            is_ble_device_id:false
         }
         this.manager = new BleManager();
         this.connectedDevice = null;
@@ -44,14 +43,15 @@ export default class unlockDevice extends React.Component {
             }
 
             if (device.id == this.state.device.ble_device_id) {
-                this.state.is_ble_device_id=true;
                 console.log("found device")
                 this.manager.stopDeviceScan()
 
                 // Connect to device
+                console.log("attempting connection")
                 await device.connect()
                     .then(device => device.discoverAllServicesAndCharacteristics() )
-                    .catch(console.log);
+                    .catch(e => console.log("Error while connecting", e));
+                console.log("connected to device")
                 this.connectedDevice = device;
                 this.setState({ ...this.state, connected: 1 })
 
@@ -61,9 +61,6 @@ export default class unlockDevice extends React.Component {
                     const message = Buffer.from(ch.value, 'base64').toString('utf8');
                     console.log("RX: " + message);
                 });
-            }
-            else{
-                this.state.is_ble_device_id=false;
             }
             return;
         });
@@ -91,9 +88,9 @@ export default class unlockDevice extends React.Component {
             <View style={unlockDeviceStyle.container}>
                 <Text style={unlockDeviceStyle.connectedText}>{this.connectedText()}</Text>                
                 <HMButton
-                    title={this.state.is_ble_device_id?"TOGGLE LIGHT":"UNLOCK DEVICE"}
+                    title={this.state.device.device_type=='ble_light'?"TOGGLE LIGHT":"UNLOCK DEVICE"}
                     displayImage={false}
-                    onPress={this.state.is_ble_device_id?Actions.pop():this.unlockBleDevice.bind(this)}
+                    onPress={this.unlockBleDevice.bind(this)}
                 />
                 <HMButton
                     title={"FORGET DEVICE"}
